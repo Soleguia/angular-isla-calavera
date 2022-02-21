ebfd3f2a242f453bd2b065a0b1aa3cc376c931a4
Josu <soleguia@gmail.com>
2022-02-21 20:28:25 +0100

    Visible active player

--- src/app/points/points.component.ts
+++ src/app/points/points.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { GameDataEntity } from '../model/game-data-entity';
 import { PlayerEntity } from '../model/player-entity';
 import { DataService } from '../services/data.service';
 
@@ -13,16 +14,22 @@ export class PointsComponent implements OnInit {
   data:DataService;
   playersData:PlayerEntity[] = [];
   playersData$:Observable<PlayerEntity[]>;
+  gameData:GameDataEntity;
+  gameData$:Observable<GameDataEntity>;
   currentPlayer:number
 
   constructor(dataService:DataService) {
     this.data = dataService
-    this.currentPlayer = this.data.gameData.player;
+    this.gameData = this.data.gameData;
   }
 
   ngOnInit(): void {
     this.playersData$ = this.data.getPlayers$();
     this.playersData$.subscribe(playersData => this.playersData = playersData);
+
+    this.gameData$ = this.data.getGameData$();
+    this.gameData$.subscribe(gameData => this.gameData = gameData);
+
   }
 
 
