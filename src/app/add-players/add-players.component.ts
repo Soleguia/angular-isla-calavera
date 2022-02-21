2071c93e74810a29ff92a83a41556ed36c86d0a5
Josu <soleguia@gmail.com>
2022-02-21 20:30:14 +0100

    Added Utils Service

--- src/app/add-players/add-players.component.ts
+++ src/app/add-players/add-players.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { UtilsService } from '../services/utils.service';
 
 @Component({
   selector: 'app-add-players',
@@ -7,18 +8,19 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./add-players.component.scss']
 })
 export class AddPlayersComponent {
-
   data:DataService;
   newName = '';
   addPlayerButtonDisabled:boolean = true;
+  private utils:UtilsService;
 
-  constructor( dataService:DataService ){
+  constructor( utilsService: UtilsService, dataService:DataService ){
+    this.utils = utilsService;
     this.data = dataService;
   }
 
   addPlayer( name:string ):void {
     if( name.length < 2){
-      name = 'Player-' + this.data.players.length + this.data.getRandom(1, 100).toFixed(0);
+      name = 'Player-' + this.data.players.length + this.utils.getRandom(1, 100).toFixed(0);
     }
     this.data.addPlayer( name );
     this.newName = '';
