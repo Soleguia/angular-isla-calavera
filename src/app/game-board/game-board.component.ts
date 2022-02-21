4f96815207591103fba86c2e382cf6322e41bcb3
Josu <soleguia@gmail.com>
2022-02-21 20:31:21 +0100

    Added Roll features

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CardEntity } from '../model/card-entity';
 import { GameDataEntity } from '../model/game-data-entity';
 import { CardService } from '../services/card.service';
 import { DataService } from '../services/data.service';
+import { UtilsService } from '../services/utils.service';
 
 @Component({
   selector: 'app-game-board',
@@ -11,34 +14,64 @@ import { DataService } from '../services/data.service';
 export class GameBoardComponent implements OnInit {
   data:DataService;
   cards:CardService;
+  currentCard:CardEntity;
   dicePool;
   gameData: GameDataEntity;
+  gameData$:Observable<GameDataEntity>;
+  utils: UtilsService;
 
   @Input()
   gameOn:boolean;
 
 
-  constructor(dataService:DataService, cardsData:CardService) {
+  constructor(utilsService: UtilsService, dataService:DataService, cardsData:CardService) {
+    this.utils = utilsService;
     this.data = dataService;
     this.cards = cardsData;
+    this.currentCard = this.cards.cardDefault;
     this.dicePool = this.data.dicePool;
     this.gameData = this.data.gameData;
   }
 
   ngOnInit(): void {
-
+    this.gameData$ = this.data.getGameData$();
+    this.gameData$.subscribe(gameData => {
+      this.gameData = gameData;
+      this.dicePool = this.gameData.throws;
+    });
   }
+
+  // First play
   firstRoll():void {
-    this.data.nextRound();
-    this.cards.currentCard = this.cards.getCard();
+    this.data.firstRound();
+    this.revealCard();
+    this.roll();
+  }
+
+  revealCard():void {
+    this.currentCard = this.cards.getCard();
+    this.gameData.card = this.currentCard;
   }
 
   roll() {
+    let dicePool = [];
+    for( let d = 0; d < this.data.poolSize; d++ ){
+      dicePool.push( this.utils.getRandom(0, 5) );
+    }
+    this.dicePool = dicePool;
+
+    this.data.setThrow({
+      round: this.gameData.round,
+      player: this.gameData.player,
+      card: this.gameData.card,
+      throws: this.dicePool
+    });
 
   }
 
   settleDown(){
     this.data.nextRound();
+    this.revealCard();
 
   }
 
